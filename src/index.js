6d5246aa7b76f1a55beab120e49ed6303a297f9d
Luisa <luisa.barros.ma9@gmail.com>
2019-11-21 17:36:53 -0300

    feat: add loading to user starred repos page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 import React from 'react';
 
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 
 import Routes from './routes';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#F36B7F" />
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {ActivityIndicator} from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -17,7 +18,7 @@ import {
 } from './styles';
 
 export default class User extends Component {
-  static navigationOptions = ({ navigation }) => ({
+  static navigationOptions = ({navigation}) => ({
     title: navigation.getParam('user').name,
   });
 
@@ -29,44 +30,65 @@ export default class User extends Component {
 
   state = {
     stars: [],
-  }
+    loading: false,
+  };
 
   async componentDidMount() {
-    const { navigation } = this.props;
+    this.callAPI();
+  }
+
+  async callAPI() {
+    const {navigation} = this.props;
     const user = navigation.getParam('user');
+
+    this.setState({loading: true});
 
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({
+      stars: response.data,
+      loading: false,
+    });
   }
 
   render() {
-    const { navigation } = this.props;
-    const { stars } = this.state;
+    const {navigation} = this.props;
+    const {stars, loading} = this.state;
 
     const user = navigation.getParam('user');
 
     return (
       <Container>
         <Header>
-          <Avatar source={{ uri: user.avatar }} />
+          <Avatar source={{uri: user.avatar}} />
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loading === true ? (
+          <ActivityIndicator
+            size="large"
+            color="#F36B7F"
+            style={{paddingTop: 20}}
+          />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={star => String(star.id)}
+            renderItem={({item}) => (
+              <Starred>
+                <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+            onRefresh={() => this.callAPI()} // Função dispara quando o usuário arrasta a lista pra baixo
+            refreshing={loading}
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={this.loadMore}
+          />
+        )}
       </Container>
     );
   }
